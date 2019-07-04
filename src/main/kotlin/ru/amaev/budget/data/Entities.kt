004fdb69ad8fca95d62348bb1b037a0ca98796df
mvamaev <amaev_mv@mail.ru>
2019-07-05 00:03:34 +0300

    fix SequenceGenerator step

--- src/main/kotlin/ru/amaev/budget/data/Entities.kt
+++ src/main/kotlin/ru/amaev/budget/data/Entities.kt
@@ -7,7 +7,7 @@ import javax.persistence.*
 
 
 @Entity
-@SequenceGenerator(initialValue = 1, name = "idgen", sequenceName = "account_seq")
+@SequenceGenerator(initialValue = 1, name = "idgen", sequenceName = "account_seq", allocationSize = 1)
 data class Account @JsonCreator constructor(@ManyToOne(fetch = FetchType.LAZY)
                                             @JoinColumn(name = "created_by", referencedColumnName = "id") var userAccount: User?,
 //                                       @ManyToMany(fetch = FetchType.LAZY)
@@ -18,18 +18,18 @@ data class Account @JsonCreator constructor(@ManyToOne(fetch = FetchType.LAZY)
     : AbstractJpaEntity<Long>(), Serializable
 
 @Entity
-@SequenceGenerator(initialValue = 1, name = "idgen", sequenceName = "category_seq")
+@SequenceGenerator(initialValue = 1, name = "idgen", sequenceName = "category_seq", allocationSize = 1)
 data class Category @JsonCreator constructor(@Column(name = "category_name") var categoryName: String?,
                                              @Column(name = "type") var type: CategoryType?)
     : AbstractJpaEntity<Long>(), Serializable
 
 @Entity
-@SequenceGenerator(initialValue = 1, name = "idgen", sequenceName = "currency_seq")
+@SequenceGenerator(initialValue = 1, name = "idgen", sequenceName = "currency_seq", allocationSize = 1)
 data class Currency @JsonCreator constructor(@Column(name = "currency_name") var currencyName: String?)
     : AbstractJpaEntity<Long>(), Serializable
 
 @Entity
-@SequenceGenerator(initialValue = 1, name = "idgen", sequenceName = "transaction_seq")
+@SequenceGenerator(initialValue = 1, name = "idgen", sequenceName = "transaction_seq", allocationSize = 1)
 data class Transaction @JsonCreator constructor(@Column(name = "amount") var amount: BigDecimal?,
                                                 @ManyToOne(fetch = FetchType.LAZY)
                                                 @JoinColumn(name = "account_id") var account: Account?,
@@ -38,14 +38,12 @@ data class Transaction @JsonCreator constructor(@Column(name = "amount") var amo
     : AbstractJpaEntity<Long>(), Serializable
 
 @Entity(name = "usr")
-@SequenceGenerator(initialValue = 1, name = "idgen", sequenceName = "usr_seq")
+@SequenceGenerator(initialValue = 1, name = "idgen", sequenceName = "usr_seq", allocationSize = 1)
 data class User @JsonCreator constructor(@Column(name = "first_name") var firstName: String?,
                                          @Column(name = "last_name") var lastName: String?,
                                          @Column(name = "middle_name") var middleName: String?,
                                          @Column(name = "role") var role: Role?)
-    : AbstractJpaEntity<Long>(), Serializable {
-
-}
+    : AbstractJpaEntity<Long>(), Serializable
 
 enum class Role { USER, ADMIN }
 
